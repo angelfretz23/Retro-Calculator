d2a761efaf6113243a16d1d16dfb2ba62ced3c1d
angelfretz23 <angel.contreras23@me.com>
2016-03-17 03:28:51 -0700

    Created pressedOperation function

--- Retro Calculator/ViewController.swift
+++ Retro Calculator/ViewController.swift
@@ -11,9 +11,23 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    enum Operation: String{
+        case Divide = "/"
+        case Multiply = "*"
+        case Subract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
     @IBOutlet weak var outputLbl: UILabel!
     
     var btnSound: AVAudioPlayer!
+    var currentOperation:Operation = Operation.Empty
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +44,62 @@ class ViewController: UIViewController {
     }
     
     @IBAction func numberPressed(btn: UIButton!){
-        btnSound.play()
+        playSound()
+        appendToOutput(btn.tag)
+        //TODO: Left of on 42 mins
+        
     }
     
+    @IBAction func onDividePress(btn: UIButton!) {
+        pressedOperation(Operation.Divide)
+        rightValStr = self.runningNumber
+        runningNumber = ""
+        currentOperation = Operation.Divide
+    }
+    
+    @IBAction func onMultiplyPressed(sender: UIButton) {
+        pressedOperation(Operation.Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: UIButton) {
+        pressedOperation(Operation.Subract)
+    }
+    
+    @IBAction func onAddPressed(sender: UIButton) {
+        pressedOperation(Operation.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: UIButton) {
+        pressedOperation(Operation.Equals)
+        var equals: Float?
+        equals = Float(rightValStr)! / Float(runningNumber)!
+        outputLbl.text = "\(equals!)"
+        equals = nil
+        reset()
+    }
 
+    
+    func appendToOutput(btnValue: Int){
+        self.runningNumber += "\(btnValue)"
+        outputLbl.text = runningNumber
+    }
+    
+    func reset(){
+        rightValStr = ""
+        leftValStr = ""
+        runningNumber = ""
+        currentOperation = Operation.Empty
+    }
+    
+    func pressedOperation(op:Operation){
+        playSound()
+    }
+    
+    func playSound(){
+        if btnSound.playing{
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
 }
 
